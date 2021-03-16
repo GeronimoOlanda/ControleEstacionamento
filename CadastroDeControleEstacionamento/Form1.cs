06ede852df8dd2bcf0d9c891915ea891df8be876
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-03-16 19:51:27 -0300

    Criando a funcionalidade de  listar e cadastrar os dados

--- CadastroDeControleEstacionamento/Form1.cs
+++ CadastroDeControleEstacionamento/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Microsoft.VisualBasic; //importando componentes do visual basic
 namespace CadastroDeControleEstacionamento
 {
     public partial class Form1 : Form
@@ -24,6 +24,8 @@ namespace CadastroDeControleEstacionamento
             bancoDeDados.Columns.Add("Entrada", typeof(string));//adicionamos uma coluna chamada entrada e a convertemos para o tipo string(texto)
 
             dataGridViewCarrosEstacionados.DataSource = bancoDeDados; //atrelamos o dataGridView ao DataTable
+            
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -33,7 +35,12 @@ namespace CadastroDeControleEstacionamento
 
         private void buttonCadastrar_Click(object sender, EventArgs e)
         {
-
+           var placa = Interaction.InputBox("DIgite a placa do veiculo:", "Entrada de veiculo");
+            if (!string.IsNullOrEmpty(placa))//verifica se a placa é vazia ou nao, para saber se o usuario clicou em OK ou não
+            {
+                bancoDeDados.Rows.Add(new string[] { placa, DateTime.Now.ToString()});
+                dataGridViewCarrosEstacionados.Rows[dataGridViewCarrosEstacionados.Rows.Count - 1].MinimumHeight = 30;
+            }
         }
     }
 }
