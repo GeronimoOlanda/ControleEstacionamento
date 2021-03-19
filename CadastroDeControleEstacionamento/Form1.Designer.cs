ab61a901b89738a8e82fc36d387ec3d930177224
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-03-19 03:44:24 -0300

    Changing the backgroud

--- CadastroDeControleEstacionamento/Form1.Designer.cs
+++ CadastroDeControleEstacionamento/Form1.Designer.cs
@@ -31,16 +31,16 @@ namespace CadastroDeControleEstacionamento
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridViewCarrosEstacionados = new System.Windows.Forms.DataGridView();
+            this.labelValorArrecadado = new System.Windows.Forms.Label();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.buttonCadastrar = new System.Windows.Forms.Button();
             this.buttonConfigurar = new System.Windows.Forms.Button();
-            this.labelValorArrecadado = new System.Windows.Forms.Label();
-            this.dataGridViewCarrosEstacionados = new System.Windows.Forms.DataGridView();
             this.labelValorHora = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
-            this.tableLayoutPanel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCarrosEstacionados)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -70,75 +70,14 @@ namespace CadastroDeControleEstacionamento
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.Controls.Add(this.dataGridViewCarrosEstacionados, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.labelValorArrecadado, 0, 1);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(222, 3);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(223, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 2;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.35802F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.641975F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(568, 405);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(567, 405);
             this.tableLayoutPanel2.TabIndex = 0;
-            // 
-            // tableLayoutPanel3
-            // 
-            this.tableLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tableLayoutPanel3.ColumnCount = 1;
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.Controls.Add(this.buttonCadastrar, 0, 0);
-            this.tableLayoutPanel3.Controls.Add(this.buttonConfigurar, 0, 2);
-            this.tableLayoutPanel3.Controls.Add(this.labelValorHora, 0, 1);
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
-            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
-            this.tableLayoutPanel3.RowCount = 3;
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.70997F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 84.29003F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 73F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(213, 405);
-            this.tableLayoutPanel3.TabIndex = 1;
-            // 
-            // buttonCadastrar
-            // 
-            this.buttonCadastrar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonCadastrar.BackColor = System.Drawing.Color.PaleGreen;
-            this.buttonCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonCadastrar.Location = new System.Drawing.Point(3, 3);
-            this.buttonCadastrar.Name = "buttonCadastrar";
-            this.buttonCadastrar.Size = new System.Drawing.Size(207, 46);
-            this.buttonCadastrar.TabIndex = 0;
-            this.buttonCadastrar.Text = "Cadastrar";
-            this.buttonCadastrar.UseVisualStyleBackColor = false;
-            this.buttonCadastrar.Click += new System.EventHandler(this.buttonCadastrar_Click);
-            // 
-            // buttonConfigurar
-            // 
-            this.buttonConfigurar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonConfigurar.BackColor = System.Drawing.Color.PaleGreen;
-            this.buttonConfigurar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonConfigurar.Location = new System.Drawing.Point(3, 334);
-            this.buttonConfigurar.Name = "buttonConfigurar";
-            this.buttonConfigurar.Size = new System.Drawing.Size(207, 68);
-            this.buttonConfigurar.TabIndex = 1;
-            this.buttonConfigurar.Text = "Configurar";
-            this.buttonConfigurar.UseVisualStyleBackColor = false;
-            this.buttonConfigurar.Click += new System.EventHandler(this.buttonConfigurar_Click);
-            // 
-            // labelValorArrecadado
-            // 
-            this.labelValorArrecadado.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.labelValorArrecadado.AutoSize = true;
-            this.labelValorArrecadado.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelValorArrecadado.Location = new System.Drawing.Point(334, 377);
-            this.labelValorArrecadado.Name = "labelValorArrecadado";
-            this.labelValorArrecadado.Size = new System.Drawing.Size(231, 19);
-            this.labelValorArrecadado.TabIndex = 0;
-            this.labelValorArrecadado.Text = "Valor da Arrecadação R$ 0.00";
-            this.labelValorArrecadado.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // dataGridViewCarrosEstacionados
             // 
@@ -156,9 +95,70 @@ namespace CadastroDeControleEstacionamento
             this.dataGridViewCarrosEstacionados.Name = "dataGridViewCarrosEstacionados";
             this.dataGridViewCarrosEstacionados.ReadOnly = true;
             this.dataGridViewCarrosEstacionados.RowHeadersVisible = false;
-            this.dataGridViewCarrosEstacionados.Size = new System.Drawing.Size(562, 363);
+            this.dataGridViewCarrosEstacionados.Size = new System.Drawing.Size(561, 363);
             this.dataGridViewCarrosEstacionados.TabIndex = 1;
             this.dataGridViewCarrosEstacionados.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewCarrosEstacionados_CellDoubleClick);
+            // 
+            // labelValorArrecadado
+            // 
+            this.labelValorArrecadado.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelValorArrecadado.AutoSize = true;
+            this.labelValorArrecadado.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelValorArrecadado.Location = new System.Drawing.Point(333, 377);
+            this.labelValorArrecadado.Name = "labelValorArrecadado";
+            this.labelValorArrecadado.Size = new System.Drawing.Size(231, 19);
+            this.labelValorArrecadado.TabIndex = 0;
+            this.labelValorArrecadado.Text = "Valor da Arrecadação R$ 0.00";
+            this.labelValorArrecadado.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.buttonCadastrar, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.buttonConfigurar, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.labelValorHora, 0, 1);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.70997F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 84.29003F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 73F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(214, 405);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // buttonCadastrar
+            // 
+            this.buttonCadastrar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCadastrar.BackColor = System.Drawing.Color.PaleGreen;
+            this.buttonCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCadastrar.Location = new System.Drawing.Point(3, 3);
+            this.buttonCadastrar.Name = "buttonCadastrar";
+            this.buttonCadastrar.Size = new System.Drawing.Size(208, 46);
+            this.buttonCadastrar.TabIndex = 0;
+            this.buttonCadastrar.Text = "Cadastrar";
+            this.buttonCadastrar.UseVisualStyleBackColor = false;
+            this.buttonCadastrar.Click += new System.EventHandler(this.buttonCadastrar_Click);
+            // 
+            // buttonConfigurar
+            // 
+            this.buttonConfigurar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonConfigurar.BackColor = System.Drawing.Color.PaleGreen;
+            this.buttonConfigurar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonConfigurar.Location = new System.Drawing.Point(3, 334);
+            this.buttonConfigurar.Name = "buttonConfigurar";
+            this.buttonConfigurar.Size = new System.Drawing.Size(208, 68);
+            this.buttonConfigurar.TabIndex = 1;
+            this.buttonConfigurar.Text = "Configurar";
+            this.buttonConfigurar.UseVisualStyleBackColor = false;
+            this.buttonConfigurar.Click += new System.EventHandler(this.buttonConfigurar_Click);
             // 
             // labelValorHora
             // 
@@ -168,7 +168,7 @@ namespace CadastroDeControleEstacionamento
             this.labelValorHora.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelValorHora.Location = new System.Drawing.Point(3, 312);
             this.labelValorHora.Name = "labelValorHora";
-            this.labelValorHora.Size = new System.Drawing.Size(207, 19);
+            this.labelValorHora.Size = new System.Drawing.Size(208, 19);
             this.labelValorHora.TabIndex = 2;
             this.labelValorHora.Text = "Valor da Hora R$ 0.00";
             this.labelValorHora.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -177,10 +177,11 @@ namespace CadastroDeControleEstacionamento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(808, 435);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "Controle de Entradas e Saidas";
@@ -188,9 +189,9 @@ namespace CadastroDeControleEstacionamento
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCarrosEstacionados)).EndInit();
             this.tableLayoutPanel3.ResumeLayout(false);
             this.tableLayoutPanel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCarrosEstacionados)).EndInit();
             this.ResumeLayout(false);
 
         }
